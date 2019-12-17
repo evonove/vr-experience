42b3317f84ce03ee857e7a9b61cb807c28530f59
Ilaria Cuccaro <ilaria96.pg@gmail.com>
2019-12-17 10:37:34 +0100

    Added Platformer scene

---
+++ Platformer.gd
@@ -0,0 +1,14 @@
+extends KinematicBody
+
+var vel = Vector3()
+var force = 9.81*10
+var pos = Vector3(0,1,0)
+var move = false
+
+func _on_Area_body_entered(body):
+	move = true
+	
+func _physics_process(delta):
+	if move:
+		vel.y = force* delta
+		move_and_slide(vel, pos)
