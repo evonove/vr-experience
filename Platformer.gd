82fae74730e84e08b94693e7cd5e8ef34351e1ce
Ilaria Cuccaro <ilaria96.pg@gmail.com>
2020-01-29 11:25:55 +0100

    modify grass

--- Platformer.gd
+++ Platformer.gd
@@ -54,21 +54,21 @@ enum queue {
 }
 
 func timer_setup():
-    """
-    setup a timer used to make the platform to blink
-    """
-    timer.set_autostart(true)
-    timer.set_wait_time(2)
-    # manually add timer to the tree
-    add_child(timer)
-    
-    
+	"""
+	setup a timer used to make the platform to blink
+	"""
+	timer.set_autostart(true)
+	timer.set_wait_time(2)
+	# manually add timer to the tree
+	add_child(timer)
+	
+	
 func _on_timer_timeout():
-    if not platform_moved:
-        # makes the platform blink
-        area_mesh_instance.visible = !area_mesh_instance.visible
-    
-    
+	if not platform_moved:
+		# makes the platform blink
+		area_mesh_instance.visible = !area_mesh_instance.visible
+	
+	
    
 func _ready():
 	timer.connect("timeout", self, "_on_timer_timeout")
@@ -83,13 +83,13 @@ func _ready():
    
 func _move_platform_with_button():
 	 timer.stop()
-	 player.set_translation(Vector3(1,0,0.75))
+	 player.set_translation(Vector3(0.5,0,0.35))
 	 is_platform_moving = true
 	 platform_moved = true
 	 area_mesh_instance.visible = false
 	 PORT.write("h")
 	 PORT.flush()
-    
+	
 func _physics_process(delta):  
 	if is_platform_moving:
 		# makes the platform move
@@ -97,7 +97,7 @@ func _physics_process(delta):
 		move_and_slide(vel, pos)
 		h = int(self.get_translation().y)
 		# platform stops
-		if h == stopping + 0.1:
+		if h == stopping:
 			force = 0
 			PORT.write("l")
 			PORT.flush()
